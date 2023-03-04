41a8d51077483283b80731907215f062640fbd4f
RobinLB99 <RJLB991102@hotmail.com>
2023-03-03 20:22:07 -0500

    Se implementa sistema dia/noche

--- themes.js
+++ themes.js
@@ -1,36 +1,51 @@
-// Seleccionar la lista desplegable y el cuerpo del documento
-const themeSelect = document.getElementById('themes_sel');
-const body = document.querySelector("body")
+window.addEventListener("load", () => {
+	const selectElement = document.querySelector('#themes_sel');
+	const root = document.documentElement;
 
-// Agregar un event listener a la lista desplegable
-themeSelect.addEventListener('change', function() {
-	setTheme(themeSelect.value);
-});
-
-// Función para cambiar el tema de la página
-function setTheme(theme) {
-	if (theme === 'light') {
-		body.classList.toggle('light');
-
-	} else if (theme === 'dark') {
-		body.classList.toggle("dark");
-	} else {
-		// Si se selecciona automático, se utiliza el tema predeterminado del usuario
-		const defaultTheme = getDefaultTheme();
-		setTheme(defaultTheme);
+	const lightColors = (element) => {
+		element.style.setProperty('--color0', '#000');
+		element.style.setProperty('--color1', '#fff');
+		element.style.setProperty('--color2', 'rgb(243, 243, 243)');
+		element.style.setProperty('--color3', 'rgb(224, 224, 224)');
+		element.style.setProperty('--color4', 'rgb(184, 184, 184)');
+		element.style.setProperty('--color5', 'rgb(168, 168, 168)');
+		element.style.setProperty('--color6', 'rgb(3, 106, 196)');
+		element.style.setProperty('--color7', 'rgb(4, 115, 206)');
+		element.style.setProperty('--color8', 'rgb(4, 129, 224)');
 	}
-}
 
-// Función para obtener el tema predeterminado del usuario
-function getDefaultTheme() {
-	// Se utiliza la API de preferencias de color para obtener el tema del usuario
-	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		return 'dark';
-	} else {
-		return 'light';
+	const darkColors = (element) => {
+		element.style.setProperty('--color0', '#fff');
+		element.style.setProperty('--color1', '#000');
+		element.style.setProperty('--color2', 'rgb(59, 59, 59)');
+		element.style.setProperty('--color3', 'rgb(51, 51, 51)');
+		element.style.setProperty('--color4', 'rgb(43, 43, 43)');
+		element.style.setProperty('--color5', 'rgb(32, 32, 32)');
+		element.style.setProperty('--color6', 'rgb(3, 106, 196)');
+		element.style.setProperty('--color7', 'rgb(4, 115, 206)');
+		element.style.setProperty('--color8', 'rgb(4, 129, 224)');
 	}
-}
 
-// Al cargar la página, se establece el tema predeterminado del usuario
-const defaultTheme = getDefaultTheme();
-setTheme(defaultTheme);
+	const readTheme = (theme) => {
+		if (theme === 'light') {
+			lightColors(root)
+		} else if (theme === 'dark') {
+			darkColors(root)
+		} else if (theme === 'auto') {
+			if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				// El usuario tiene habilitado el tema oscuro en su sistema operativo
+				darkColors(root)
+			} else {
+				// El usuario no tiene habilitado el tema oscuro en su sistema operativo
+				lightColors(root)
+			}
+		}
+	}
+
+	readTheme(selectElement.value);
+
+	selectElement.addEventListener("change", (event) => {
+		const theme = event.target.value;
+		readTheme(theme);
+	});
+})
